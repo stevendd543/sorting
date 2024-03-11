447dffcfbca22fcae31e60f12e9aa599a05f84bd
stevendd <stevendd543@gmail.com>
2024-03-11 13:06:15 +0800

    revise stack representation

--- main.c
+++ main.c
@@ -4,9 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "list.h"
-
+#include <time.h>
 typedef struct __node {
-    struct __node *left, *right;
     struct list_head list;
     long value;
 } node_t;
@@ -57,28 +56,42 @@ bool cmp(struct list_head *a,int value)
     return list_entry(a, node_t, list)->value > value;
 }
 void quick_sort(node_t *head) {
+    clock_t time = clock();
     if (!head || list_empty(&head->list))
         return;
-    int n = list_length(&head->list);
-    int max_level = 2 * n;
+    // int n = list_length(&head->list);
+    // int max_level = n;
+    // max_level=100;
     int i = 0;
+    int level_count = 0;
+    // struct list_head *begin[max_level], *end[max_level];
+    struct list_head **begin = malloc(sizeof(struct list_head*)), **end = malloc(sizeof(struct list_head*));
 
-    struct list_head *begin[max_level], *end[max_level];
     struct list_head *result = malloc(sizeof(struct list_head));
     struct list_head *left = malloc(sizeof(struct list_head)); 
     struct list_head *right = malloc(sizeof(struct list_head));
     struct list_head *ppviot = malloc(sizeof(struct list_head));
 
+
     INIT_LIST_HEAD(result);
 
+    INIT_LIST_HEAD(*begin);
+        
+    INIT_LIST_HEAD(*end);    
 
-    begin[0] = head->list.next;
-    end[0] = head->list.prev;
+    struct list_head *next = head->list.next, *prev = head->list.prev;
+    list_add__(begin,next);
+    list_add__(end, prev);
+ 
+    // begin[0] = head->list.next;
+    // end[0] = head->list.prev;
 
-    while (i >=0) {
-        struct list_head *L = begin[i], *R = end[i];
+    while (i >= 0) {
+        struct list_head *L = list_tail(*begin), *R = list_tail(*end);
+        level_count = level_count > i ? level_count : i;
         
         if (L != R) {
+
             struct list_head *pivot = L;
             int value = list_entry(pivot, node_t, list)->value;
             // printf("value: %d\n",value);
@@ -90,7 +103,7 @@ void quick_sort(node_t *head) {
 
             list_add__(ppviot, pivot);
 
-
+            // exit(1);
             struct list_head *stop = p->prev;
             INIT_LIST_HEAD(left);
             INIT_LIST_HEAD(right);
@@ -98,20 +111,43 @@ void quick_sort(node_t *head) {
                 struct list_head *n = p;
                 p = p->next;
                 list_del(n);
-                // break;
                 list_add__(cmp(n, value) ? right : left, n);
-            }        
+            }
+            if(!list_empty(left)) {
+                struct list_head *n = left->next;
+                struct list_head *p = left->prev;
+                list_add_tail(*begin, n);
+                list_add_tail(*end, p);
+            }
 
-            begin[i] = list_empty(left) ? NULL : left->next;
-            end[i] = list_empty(left) ? NULL : list_tail(left);
-            begin[i + 1] = ppviot->next;
-            end[i + 1] = list_tail(ppviot);
-            begin[i + 2] = list_empty(right) ? NULL : right->next;
-            end[i + 2] = list_empty(right) ? NULL : list_tail(right);
+            list_add_tail(begin, ppviot->next);
+            list_add_tail(end, list_tail(ppviot));
+
+            if(!list_empty(right)) {
+                struct list_head *n = right->next;
+                struct list_head *p = right->prev;
+                list_add_tail(begin, n);
+                list_add_tail(end, p);
+            }    
+
+ 
+    
+            // begin[i] = list_empty(left) ? NULL : left->next;
+            // end[i] = list_empty(left) ? NULL : list_tail(left);
+            // begin[i + 1] = ppviot->next;
+            // end[i + 1] = list_tail(ppviot);
+            // begin[i + 2] = list_empty(right) ? NULL : right->next;
+            // end[i + 2] = list_empty(right) ? NULL : list_tail(right);
             i += 2;
         } else {
+            printf("1");
             if (L) 
                 list_add__(result, L);
+            list_del_init(begin->prev);
+            list_del_init(end->prev);
+             
+
+            // free()
             i--;
         }
     }
@@ -121,6 +157,8 @@ void quick_sort(node_t *head) {
     free(right);
     free(result);
     free(ppviot);
+    time = clock() - time;
+    printf("max level : %d\t cpu clock time : %ld\n",level_count,time);
 }
 
 
@@ -147,15 +185,15 @@ static bool list_is_ordered(node_t *head)
 }
 
 /* shuffle array, only work if n < RAND_MAX */
-void print_sort(node_t *p)
-{
-    if(!p)
-        return;
-    node_t *entry;
-    list_for_each_entry(entry, &p->list, list){
-        printf("%ld\n",entry->value);
-    }
-}
+// void print_sort(node_t *p)
+// {
+//     if(!p)
+//         return;
+//     node_t *entry;
+//     list_for_each_entry(entry, &p->list, list){
+//         printf("%ld\n",entry->value);
+//     }
+// }
 void shuffle(int *array, size_t n)
 {
     if (n <= 0)
@@ -171,7 +209,7 @@ void shuffle(int *array, size_t n)
 
 int main(int argc, char **argv)
 {
-    size_t count = 100000;
+    size_t count = 1000;
 
     int *test_arr = malloc(sizeof(int) * count);
 
